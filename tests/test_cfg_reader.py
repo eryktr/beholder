741d7463f7e9b24e611ba26c4966f3aa9d0a235f
TeeKay18 <tommkar321@wp.pl>
2020-03-28 14:50:46 +0100

    File reader v2

--- tests/test_cfg_reader.py
+++ tests/test_cfg_reader.py
@@ -1,45 +1,36 @@
 from itertools import permutations
 from pathlib import Path
-from beholder.cfg_reader import correct_protocol, valid_arguments
-from beholder.cfg_reader import correct_websites
-from beholder.cfg_reader import valid_file
+from beholder.cfg_reader import protocol_is_correct, arguments_are_valid
+from beholder.cfg_reader import find_incorrect_websites
+# from beholder.cfg_reader import file_is_valid
 
 
-def test_corr_prot():
+def test_protocol_is_correct():
     rand_comb = [''.join(p) for p in permutations('eryktr')]
     for comb in rand_comb:
-        assert correct_protocol(comb.join(["https://", ".pl"]))
+        assert protocol_is_correct(comb.join(["https://", ".pl"]))
+        assert not(protocol_is_correct(comb.join(".pl")))
 
 
-def test_corr_prot_wrong():
-    rand_comb = [''.join(p) for p in permutations('eryktr')]
-    for comb in rand_comb:
-        assert not(correct_protocol(comb.join(".pl")))
-
-
-def test_val_arg_correct():
+def test_arguments_are_valid():
     cwd = Path.cwd()
     arguments = ["cfg_reader.py", "-t", "5050"]
     arguments.append(Path.joinpath(cwd, "tests", "correct.txt"))
-    assert valid_arguments(arguments)
+    other_arguments = ["cfg_reader.py", "-t", "05050"]
+    assert arguments_are_valid(arguments)
+    assert not(arguments_are_valid(other_arguments))
 
 
-def test_val_arg_wrong():
-    arguments = ["cfg_reader.py", "-t", "05050"]
-    arguments.append("./nosuchfile.txt")
-    assert not(valid_arguments(arguments))
+def test_find_incorrect_websites():
+    correct_list = []
+    incorrect_list = []
+    for i in range(0, 12):
+        correct_list.append("https://eryktrzeciakiewicz.pl")
+        incorrect_list.append("eryktrzeciakiewicz.pl")
+    assert not(len(find_incorrect_websites(correct_list)))
+    assert len(find_incorrect_websites(incorrect_list)) == 12
 
 
-def test_cor_websites():
+""" def test_file_is_valid():
     cwd = Path.cwd()
-    assert correct_websites(Path.joinpath(cwd, "tests", "correct.txt"))
-
-
-def test_wrong_websites():
-    cwd = Path.cwd()
-    assert not(correct_websites(Path.joinpath(cwd, "tests", "incorrect.txt")))
-
-
-def test_valid_file():
-    cwd = Path.cwd()
-    assert valid_file(Path.joinpath(cwd, "tests", "correct.txt"))
+    assert file_is_valid(Path.joinpath(cwd, "tests", "correct.txt")) """
