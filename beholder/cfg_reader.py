5f99e317f49f55b78e05f7bc8d8945679902b169
TeeKay18 <tommkar321@wp.pl>
2020-03-28 14:50:46 +0100

    File reader v2

--- beholder/cfg_reader.py
+++ beholder/cfg_reader.py
@@ -1,49 +1,24 @@
 from pathlib import Path
-import sys
+from typing import List
 
 
-def usage():
-    return ("""Usage:\n python cfg_reader.py -t (positive_number)
-    path_to_file/your_config_file""")
-
-
-def correct_protocol(addr):
+def protocol_is_correct(addr: str) -> bool:
     return addr.startswith("https://") or addr.startswith("http://")
 
 
-def valid_arguments(data):
-    return (len(data) == 4 and data[1] == "-t"
-            and data[2].isnumeric() and not(data[2].startswith("0")))
+def arguments_are_valid(args: List[str]) -> bool:
+    return len(args) in (2, 4)
 
 
-def correct_websites(file):
-    print("Checking if websites are valid...")
-    lines = [line for line in file.read_text().split('\n') if line]
-    errors = 0
-    for addr in lines:
-        if not(correct_protocol(addr)):
-            if (errors == 0):
-                print("---------------------------")
-                print("Invalid websites:")
-                print("---------------------------")
-            print(addr)
-            errors = errors + 1
-    if (errors != 0):
-        print("---------------------------")
-        print("There should be protocol https:// or http://")
-        print(f"Total amount of errors: {errors}")
-    return errors == 0
+def find_incorrect_websites(sites: List[str]) -> List[str]:
+    return [site for site in sites if not protocol_is_correct(site)]
 
 
-def valid_file(config_file):
-    return config_file.is_file() and correct_websites(config_file)
+def file_is_valid(path: Path) -> bool:
+    sites = [line for line in path.read_text().split('\n') if line]
+    return path.is_file() and not(len(find_incorrect_websites(sites)))
 
 
-def check(data):
-    valid = valid_arguments(data) and valid_file(Path(data[3]))
-    if (not(valid_arguments(data))):
-        print(usage())
-    return valid
-
-
-check(sys.argv)
+def check(data: List[str]) -> bool:
+    return (arguments_are_valid(data) and
+            (file_is_valid(Path(data[3])) or file_is_valid(Path(data[1]))))
