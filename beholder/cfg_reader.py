87f6a52898d2b1bdf877e17987800614c25a968c
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-03-28 16:31:17 +0100

    Cleaned up api

--- beholder/cfg_reader.py
+++ beholder/cfg_reader.py
@@ -10,12 +10,12 @@ def find_incorrect_websites(sites: List[str]) -> List[str]:
     return [site for site in sites if not protocol_correct(site)]
 
 
-def _parse_file(path: Path) -> List[str]:
-    return [line for line in path.read_text().split('\n') if line]
-
-
 def file_valid(path: Path) -> bool:
     if not path.is_file():
         return False
     sites = _parse_file(path)
     return not any(find_incorrect_websites(sites))
+
+
+def _parse_file(path: Path) -> List[str]:
+    return [line for line in path.read_text().split('\n') if line]
