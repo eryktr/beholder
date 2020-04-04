80a0483ad31805f729383b40a5668dec98a35e79
TeeKay18 <tommkar321@wp.pl>
2020-04-04 23:18:12 +0200

    Fetch test 100% coverage

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -13,3 +13,11 @@ def test_fetch_raises_for_invalid_statcode(mocker, monkeypatch):
 
     with pytest.raises(errors.IncorrectStatusError):
         fetcher.fetch('www.hello-world.com', pathlib.Path() / 'file.html')
+
+
+def test_fetch_correct(mocker, monkeypatch):
+    res_mock = mocker.Mock(status_code=200)
+    monkeypatch.setattr(requests, 'get', lambda addr: res_mock)
+    monkeypatch.setattr(pathlib.Path, 'write_text', mocker.Mock())
+    fetcher = WebFetcher()
+    fetcher.fetch('www.hello-world.com', pathlib.Path() / 'file.html')
