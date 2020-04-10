6ddbba36957664cc1ef062b7a6f4fdfb53ea5de0
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-04-10 20:38:38 +0200

    Improved architecture of analyzer

--- beholder/file_comparator/reporter_factory.py
+++ beholder/file_comparator/reporter_factory.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 from typing import Optional
 
-from beholder.file_comparator.reporters import BaseReporter, StdoutReporter, FileReporter
+from beholder.file_comparator.reporters import BaseReporter, StdoutReporter, FileReporter, Reporter
 
 
 class ReporterFactory:
     @staticmethod
     def create(output_path: Optional[Path] = None):
-        reporter = StdoutReporter(BaseReporter())
+        reporter: Reporter = StdoutReporter(BaseReporter())
         if output_path:
             reporter = FileReporter(reporter, output_path)
         return reporter
