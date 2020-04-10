cb08e6c76445378371485e258f6b98786d4f5001
TeeKay18 <tommkar321@wp.pl>
2020-04-10 23:48:29 +0200

    Main fix

--- beholder/main.py
+++ beholder/main.py
@@ -3,7 +3,6 @@ import beholder.argparser as argparser
 import beholder.cfg_reader as cfg_reader
 import beholder.opts_validator as opts_validator
 import beholder.analyzer.state_checker as state_checker
-import beholder.file_comparator.comparison_types as comparison_types
 
 
 def main():
@@ -11,11 +10,8 @@ def main():
     opts_validator.validate_opts(opts)
     sites = cfg_reader.parse_file(opts.config_path)
     cfg_reader.validate_websites(sites)
-    if opts.show_diffs:
-        checker = state_checker.StateChecker(comparison_types.WithDiffsComparison())
-    else:
-        checker = state_checker.StateChecker(comparison_types.SimpleComparison())
-    checker.run(opts, sites)
+    checker = state_checker.StateChecker(sites, opts)
+    checker.run()
 
 
 if __name__ == '__main__':
