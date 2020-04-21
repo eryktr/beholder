0babdc13f2e1706fc9495b74746f0491298f8653
TeeKay18 <tommkar321@wp.pl>
2020-04-21 18:55:23 +0200

    Variable name change

--- beholder/processor.py
+++ beholder/processor.py
@@ -6,5 +6,5 @@ def process(res_content: str) -> str:
     cleaner = Cleaner()
     cleaner.javascript = True
     cleaner.style = True
-    y = cleaner.clean_html(res_content)
-    return BeautifulSoup(y, "lxml").text
+    clean_text = cleaner.clean_html(res_content)
+    return BeautifulSoup(clean_text, "lxml").text
