69312d1c5bf5f1360b85e334dba161eb7b1b01f3
TeeKay18 <tommkar321@wp.pl>
2020-07-05 23:50:12 +0200

    Optimization fixes

--- tests/analyzer/test_state_checker.py
+++ tests/analyzer/test_state_checker.py
@@ -1,27 +1,38 @@
 from beholder.analyzer.site import Site
 from beholder.analyzer.state_checker import StateChecker
+from beholder.const import HTTPStatus
 from pathlib import Path
 
+import pytest
 import requests
 import beholder.processor as processor
 import beholder.reports.report_builder as report_builder
 
 
+@pytest.fixture
+def state_paths(tmp_path):
+    old_file = tmp_path / '1o.html'
+    old_file.touch()
+    new_file = tmp_path / '1n.html'
+    new_file.touch()
+    return old_file, new_file
+
+
 def test_check_site(mocker, paths, state_paths, monkeypatch):
     cfg_file, out_file = paths
     old_file, new_file = state_paths
-    site = Site("dummy", old_file, new_file)
     opts = mocker.Mock(config_path=cfg_file, output_path=out_file, time=30, show_diffs=False)
-    checker = StateChecker([site], opts, 1)
+    sites = [Site("dummy", old_file, new_file)]
+    checker = StateChecker(sites, opts)
 
-    res_mock = mocker.Mock(status_code=200)
-    monkeypatch.setattr(requests, 'get', lambda addr: res_mock)
-    monkeypatch.setattr(Path, 'write_text', mocker.Mock())
-    monkeypatch.setattr(processor, 'process', mocker.Mock())
+    res_mock = mocker.Mock(status_code=HTTPStatus.OK)
+    simple_mock = mocker.Mock()
 
-    checker._check_site(site)
-    compare = checker.comparator.compare(site.reference_path, site.update_path)
+    monkeypatch.setattr(requests, "get", lambda addr: res_mock)
+    monkeypatch.setattr(Path, "write_text", simple_mock)
+    monkeypatch.setattr(processor, "process", simple_mock)
 
-    assert "Website has not changed" in report_builder.build(site.addr,
-                                                             compare,
-                                                             with_diffs=False)
+    checker._check_site(sites[0])
+    res = checker.comparator.compare(sites[0].reference_path, sites[0].update_path)
+
+    assert "Website has not changed" in report_builder.build(sites[0].addr, res, with_diffs=False)
